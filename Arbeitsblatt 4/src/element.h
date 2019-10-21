ef8349e6f6c62ebeb331818979571148fe20c1c2
Lorenz Sommer <lorenzsommer1@gmail.com>
2019-10-21 11:48:08 +0200

    ab4

---
+++ Arbeitsblatt 4/src/element.h
@@ -0,0 +1,28 @@
+/*
+ * element.h
+ *
+ *  Created on: Nov 29, 2019
+ *      Author: lorenz
+ */
+
+#ifndef ELEMENT_H_
+#define ELEMENT_H_
+
+struct Element
+{
+	unsigned int value;
+	void* pSuccessor;
+};
+typedef struct Element Element;
+
+Element* elementCreate()
+
+{
+	Element* pElement = (Element*) malloc(sizeof(Element));
+	pElement->pSuccessor = NULL;
+	pElement->value = 0;
+	printf("elementcreate\n");
+	return pElement;
+}
+
+#endif /* ELEMENT_H_ */
