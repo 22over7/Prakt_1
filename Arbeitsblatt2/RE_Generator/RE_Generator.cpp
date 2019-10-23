ff464168aea9c1db4a4930ae2fea2290fd0aaa10
Lorenz Sommer <lorenzsommer1@gmail.com>
2019-10-23 09:01:03 +0200

    Ordner wieder durcheinander gekommen
    RE_Generator repariert - fertig

--- Arbeitsblatt2/RE_Generator/RE_Generator.cpp
+++ Arbeitsblatt2/RE_Generator/RE_Generator.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void rechteck(unsigned int a, unsigned int b, char c)
+void rechteck(unsigned int b, unsigned int h, char c)
 //a für breite, b für hoehe, c für das Zeichen
 {
 
 	//Oberste Zeile
-	for (int i = 0; i < a; i = i + 1)
+	for (int i = 1; i <= b; i = i + 1)
 	{
 		printf("%c", c);
 	}
@@ -13,36 +13,48 @@ void rechteck(unsigned int a, unsigned int b, char c)
 
 	//Körper des Rechteckes
 
-	for (int i2 = 3; i2 <= b; i2 = i2 + 1)  //Für jede Zeile ab der zweiten
+	for (int i2 = 2; i2 < h; i2 = i2 + 1)  //Für jede Zeile ab der zweiten
 	{
 		//Das erste Element einer Zeile ist immer das Zeichen also für Breitenindex 1 -> Zeichen
 		//Dann (Breite - 2) Leerzeichen
 		//Letztes Element wieder das Zeichen
 
-		for (int i3 = 1; i3 <= a; i3 = i3 + 1)
+		for (int i3 = 1; i3 <= b; i3 = i3 + 1)
 		{
-			if (i3 == 1)  //Erstes
+			if (b == 1)
+			{
+				putchar(c);
+				printf("\n");
+			}
+			else if (i3 == 1)  //Erstes
 			{
 				putchar(c);
 			}
-			else if (i3 != 1 && i3 != a)  //Leerzeichen dazwischen
+			else if (i3 != 1 && i3 != b)  //Leerzeichen dazwischen
 			{
-				putchar(' ');
+				putchar('n');
 			}
-			else if (i3 == a)
+			else if (i3 == b)
 			{
 				putchar(c);		//Letztes Zeichen
 				printf("\n");   //Absatz nach jeder Zeile
-
 			}
 		}
 	}
 
 	//Unterste Zeile
-	for (int i = 0; i < a; i = i + 1)
+	for (int i = 1; i <= b; i = i + 1)
+	{
+		if (h == 1 || h == 0)
+		{
+			break;
+		}
+		else if (h != 1)
 		{
 			printf("%c", c);
 		}
+	}
+
 
 }
 
@@ -61,7 +73,7 @@ int main(void)
 	/*printf("Zeichen? \n");
 	scanf("%c",&c);*/
 
-	rechteck(a,b,'#');
+	rechteck(5,1,'#');
 
 	return 0;
 }
